8deadf2e935924d268c96edd06298c8803385eba
jimmysi2so <101122177+jimmysi2so@users.noreply.github.com>
2022-09-22 00:02:10 -0300

    Add files via upload

---
+++ agua/e2e/src/app/ingresar.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { NavController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IngresarGuard implements CanActivate {
+
+constructor(public navCtrl: NavController){}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(localStorage.getItem('ingresar')){
+        return true;
+      }else{
+        this.navCtrl.navigateRoot('login');
+        return false;
+      }
+      
+  }
+  
+}
